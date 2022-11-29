315114b9b7959696770b4295d07ac561bc1a579e
unknown <fahimaakter2014@gmail.com>
2022-11-29 13:27:27 +0600

    create admin role

--- src/Pages/Dashboard/AllUsers/AllUsers.js
+++ src/Pages/Dashboard/AllUsers/AllUsers.js
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const AllUsers = () => {
     const { data: users = [], refetch } = useQuery({
@@ -10,23 +11,37 @@ const AllUsers = () => {
             return data;
         }
     });
+
+    const handleAdminRole = (id) => {
+        fetch(`http://localhost:5000/users/admin/${id}`, {
+            method: 'PUT'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    toast.success('Admin Created Succesfully.')
+                    refetch();
+                }
+
+            })
+
+    }
+
     return (
         <div>
             <h3 className='text-3xl font-bold text-violet-900 my-7 '>All users</h3>
             <div className="overflow-x-auto w-full">
                 <table className="table w-full">
 
-                    <thead>
+                    <thead className='text-white'>
                         <tr>
                             <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
+
                             </th>
-                            <th>Product Details</th>
-                            <th>Buyer Details</th>
-                            <th>Location</th>
+                            <th>User Details</th>
+                            <th>Email</th>
                             <th></th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody >
@@ -41,19 +56,22 @@ const AllUsers = () => {
                                     <div className="flex items-center space-x-3">
 
                                         <div>
-                                            <div className="font-semibold text-lg text-white">{user.name}</div>
-                                            <div className="text-sm  font-bold text-green-300">Price: ${user.price}</div>
+                                            <div className="font-semibold text-lg text-white">Name: {user.name}</div>
+                                            <div className="text-sm  font-bold text-green-300">Position: {user.status}</div>
                                         </div>
                                     </div>
                                 </td>
                                 <td>
                                     {user.buyer}
                                     <br />
-                                    <span className="badge badge-ghost badge-sm">Phone:{user.phone}</span>
+                                    <span className="badge badge-ghost badge-sm"> {user.email}</span>
                                 </td>
-                                <td>{user.meetPlace}</td>
+                                <td>
+                                    {user?.role !== 'admin' && <button onClick={() => handleAdminRole(user._id)} className="btn bg-green-400 btn-xs px-5 text-black">Make Admin</button>}
+                                </td>
+
                                 <th>
-                                    <button className="btn btn-ghost btn-xs">Proceed</button>
+                                    <button className="btn btn-primary bg-red-400 btn-xs">Delete</button>
                                 </th>
                             </tr>)
                         }
